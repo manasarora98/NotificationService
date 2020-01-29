0201e212b5d47e0a3b66eca45b6a76a7af92f0f7
Manas Arora <manasarora98@gmail.com>
2020-01-29 19:10:36 +0530

    NotificationService

--- repository/NotificationRepository.java
+++ repository/NotificationRepository.java
@@ -1,9 +1,15 @@
 package com.project.NotifcationService.NotificationService.repository;
 
 import com.project.NotifcationService.NotificationService.entity.NotificationEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.management.openmbean.CompositeData;
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends CrudRepository<NotificationEntity, String> {
+    List<NotificationEntity> findByUserId(String id);
+
 }
